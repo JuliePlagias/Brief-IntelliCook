224bd5e671b0457059e594e9e35cb4c3e0f06a5f
JuliePlagias <julieplagias@gmail.com>
2025-01-05 16:41:58 +0100

    Feat - modified ingredient component

--- src/component/Ingredient.jsx
+++ src/component/Ingredient.jsx
@@ -1,3 +1,4 @@
+const size = 150;
 
 export default function Ingredient({name, quantity, unit})
 {
@@ -5,11 +6,18 @@ export default function Ingredient({name, quantity, unit})
         const d = ["a", "e", "i", "o", "u", "y", "h"];
         return unit? d.find((l) => l === ingredient[0]) ? " d'" : " de ":"";
       };
+      const singularOrPlural= (quantity)=>{
+        return quantity>1? "s":"";
+      }
       
     return (
         <>
-        <img src={`/assets/images/${name}.jpg`} alt={name}/>
-        <p>{quantity} {unit} {writeGoodDeOrD(name)}{name}</p>
+        <img src={`/assets/images/${name}.jpg`} alt={name}
+        style={{
+            width:size,
+            height: size,
+            borderRadius:size/2}}/>
+        <p>{quantity} {unit} {writeGoodDeOrD(name)}{name}{singularOrPlural(quantity)}</p>
         </>
     )
 }
