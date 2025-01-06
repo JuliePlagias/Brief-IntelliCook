d2eea8bd74b9bc7befb284de75ca12537bf94533
gouttedooo <goudal.donovan@live.fr>
2025-01-06 12:15:27 +0100

    fix: images names werent good but now OK, and add eau.jpg

--- src/component/Ingredient.jsx
+++ src/component/Ingredient.jsx
@@ -1,23 +1,30 @@
 const size = 150;
 
-export default function Ingredient({name, quantity, unit})
-{
-    const writeGoodDeOrD = (ingredient) => {
-        const d = ["a", "e", "i", "o", "u", "y", "h"];
-        return unit? d.find((l) => l === ingredient[0]) ? " d'" : " de ":"";
-      };
-      const singularOrPlural= (quantity)=>{
-        return quantity>1? "s":"";
-      }
-      
-    return (
-        <>
-        <img src={`/assets/images/${name}.jpg`} alt={name}
+export default function Ingredient({ name, quantity, unit }) {
+  const writeGoodDeOrD = (ingredient) => {
+    const d = ["a", "e", "i", "o", "u", "y", "h"];
+    return unit ? (d.find((l) => l === ingredient[0]) ? " d'" : " de ") : "";
+  };
+  const singularOrPlural = (quantity) => {
+    return quantity > 1 ? "s" : "";
+  };
+
+  return (
+    <>
+      <img
+        src={`/assets/images/${name}.jpg`}
+        alt={name}
         style={{
-            width:size,
-            height: size,
-            borderRadius:size/2}}/>
-        <p>{quantity} {unit} {writeGoodDeOrD(name)}{name}{singularOrPlural(quantity)}</p>
-        </>
-    )
+          width: size,
+          height: size,
+          borderRadius: size / 2,
+        }}
+      />
+      <p>
+        {quantity} {unit} {writeGoodDeOrD(name)}
+        {name}
+        {singularOrPlural(quantity)}
+      </p>
+    </>
+  );
 }
---
+++ src/styles/README.md
@@ -0,0 +1,7 @@
+This repo is an 7-1 architecture for a project using SASS.
+
+Go to the file : `base/_base.scss` to remove the red background (it's just for testing if the css is linked).
+
+Enjoy !
+
+
