bbdf235b52807b7d49d694b54e068774ac38e2e2
gouttedooo <goudal.donovan@live.fr>
2025-01-06 14:31:12 +0100

    style: all images recipes OK

--- src/component/RecipeCard.jsx
+++ src/component/RecipeCard.jsx
@@ -3,12 +3,15 @@ import React from "react";
 const RecipeCard = ({ recipe }) => {
   return (
     <div className="recipeCard">
-      <div className="container">
-        <img
-          src={`/assets/images/recettes/${recipe.name.toLowerCase()}.jpg`}
-          alt={recipe.name}
-        />
-        <h1>{recipe.name}</h1>
+      <img
+        src={`/assets/images/recettes/${recipe.name.toLowerCase()}.jpg`}
+        alt={recipe.name}
+      />
+      <h1 className="recipeCard__name">{recipe.name}</h1>
+      <span className="recipeCard__time"> (clock) 40 min</span>
+      <div className="recipeCard__buttons">
+        <div className="recipeCard__buttons-add"></div>
+        <div className="recipeCard__buttons-favorite"></div>
       </div>
     </div>
   );
