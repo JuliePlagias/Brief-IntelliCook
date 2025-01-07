7472e3d8a0579a66f3a99cf371bea8969e31f6bb
JuliePlagias <julieplagias@gmail.com>
2025-01-07 11:13:34 +0100

    links and routes done, working on burger menu

--- src/component/home/MenuBurger.jsx
+++ src/component/home/MenuBurger.jsx
@@ -2,21 +2,20 @@ import {Link, Outlet} from "react-router-dom";
 
 export default function MenuBurger()
 {
-    console.log('Menu burger rendu');
     return (
         <>
         <div className="menuBurger">
             <img src="/assets/images/icons/logo.png" alt="Intelli Cook Logo"></img>
             <ul>
-                {/* <li>
-                    <Link to="/">Accueil</Link>
+                <li>
+                    <Link to="/">Acceuil</Link>
                 </li>
                 <li>
                     <Link to="/catalogue">Catalogue</Link>
                 </li>
                 <li>
                     <Link to="/favoris">Favoris</Link>
-                </li> */}
+                </li>
             </ul>
         </div>
         <Outlet/>
