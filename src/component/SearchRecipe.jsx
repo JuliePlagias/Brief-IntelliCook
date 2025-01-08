1ed834bc1c086cff69807d9326e1d304f3d79e11
Aissam KALI <153532806+AissamK31@users.noreply.github.com>
2025-01-08 13:44:33 +0100

    test: ajout loupe en cours de style

--- src/component/SearchRecipe.jsx
+++ src/component/SearchRecipe.jsx
@@ -1,24 +1,31 @@
-import React, { useState } from "react";
-import Recipes from "./Recipes";
+import React, { useState } from 'react'
+import Recipes from './Recipes'
 
 /**
  * Retourne toutes les recettes qui correspondent à la recherche.
  * @returns {JSX.Element} Toutes les recettes sont affichées si rien n'est entré dans l'input.
  */
 const SearchRecipe = () => {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('')
 
   return (
     <div>
       <input
-        type="text"
-        placeholder="Soupe de légumes, poireaux, etc..."
-        onChange={(e) => setSearch(e.target.value)}
-        className="searchRecipe"
+        type='text'
+        placeholder='Ratatouille, poireaux, ...'
+        onChange={e => setSearch(e.target.value)}
+        className='searchRecipe'
       />
+
+      <img
+        id='loupe'
+        src='./assets/images/icons/loupe recherche.png'
+        alt='Recherche'
+      />
+
       <Recipes search={search} />
     </div>
-  );
-};
+  )
+}
 
-export default SearchRecipe;
+export default SearchRecipe
