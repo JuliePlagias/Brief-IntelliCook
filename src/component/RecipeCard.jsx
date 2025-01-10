459b0ebd5206df382424297405a3db86b0880001
Aissam KALI <153532806+AissamK31@users.noreply.github.com>
2025-01-10 12:37:10 +0100

    styles: modif timer and favorite on recipecard

--- src/component/RecipeCard.jsx
+++ src/component/RecipeCard.jsx
@@ -60,10 +60,13 @@ const RecipeCard = ({ recipe }) => {
           viewBox='0 0 24 24'
         >
           <path
-            fill='black'
+            fill='currentColor'
             d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z'
           />
-          <path fill='black' d='M12.5 7H11v6l5.25 3.15.75-1.23-4.5-2.67z' />
+          <path
+            fill='currentColor'
+            d='M12.5 7H11v6l5.25 3.15.75-1.23-4.5-2.67z'
+          />
         </svg>
         {recipe.time >= 60 ? timeInHour(recipe.time) : recipe.time + ' min'}
       </span>
