b6a22daacde36e75f39f12503905ac606f51b977
JuliePlagias <julieplagias@gmail.com>
2025-01-10 16:39:05 +0100

    Add : moved find recipe function to a js file

--- src/component/Recipes.jsx
+++ src/component/Recipes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import recettes from '../data/recipes.json'
 import RecipeCard from './RecipeCard'
+import {findRecipeByName, findRecipeByIngredient} from "../utils/functions/searchFunctions";
 
 /**
  * Mappe toutes les recettes trouvées à partir de la recherche search
@@ -9,46 +10,15 @@ import RecipeCard from './RecipeCard'
 
 const Recipes = ({ search, searchType, filterIng }) => {
   const {addedIngredientsList, removedIngredientsList} = filterIng;
-
-  const findRecipeByName = inputValue => {
-    //Filtrer les recettes dont le nom contient la recherche (recherche par défaut sans ingrédients ajoutés ou retirés)
-    const filteredRecipesByDefault = recettes.recipes.filter(recipe =>
-      recipe.name.toLowerCase().includes(inputValue.toLowerCase())
-    );
-
-    //si addedIngredientsList est vide, retourner les recettes par défaut
-    if (addedIngredientsList.length === 0) return filteredRecipesByDefault;
-    //Filtrer les recettes dont les ingrédients indispensables sont présents
-    console.log('addedIngredientsList : ', addedIngredientsList);
-    
-    const filteredRecipesByAddedIngredients = 
-    filteredRecipesByDefault.filter((recipe) => {
-      console.log("recette testé : ", recipe);
-      for (let i = 0 ; i < addedIngredientsList.length ; i++) {
-        console.log("ingrédient testé : ", addedIngredientsList[i]);
-        if (!recipe.ingredients.map(i => i.name.toLowerCase()).includes(addedIngredientsList[i].toLowerCase())) {
-          return false;
-        }
-    }
-    return true;
-  });
-    console.log("filtered recipes by added ingredients : ", filteredRecipesByAddedIngredients);
-  
-    //Puis, dégager les recettes dont les ingrédients non désirables sont présents
-    return filteredRecipesByAddedIngredients;
-  }
-
-  const findRecipeByIngredient = inputValue => recettes.recipes.filter(recipe => recipe.ingredients.some(ingredient => ingredient.name.toLowerCase().includes(inputValue.toLowerCase())))
-
   return (
     <div className='recipes'>
-      {searchType === 'name' && findRecipeByName(search) &&
-        findRecipeByName(search).map((recipe, i) => {
+      {searchType === 'name' && findRecipeByName(search, addedIngredientsList, removedIngredientsList) &&
+        findRecipeByName(search, addedIngredientsList, removedIngredientsList).map((recipe, i) => {
           if (i < 12) {
             return <RecipeCard key={recipe.id} recipe={recipe} />
           }
         })}
-      {searchType === 'ingredients' && findRecipeByIngredient(search) &&
+      {searchType === 'ingredients' && findRecipeByIngredient(search, addedIngredientsList, removedIngredientsList) &&
         findRecipeByIngredient(search).map((recipe, i) => {
           if (i < 12) {
             return <RecipeCard key={recipe.id} recipe={recipe} />
---
+++ src/utils/functions/searchFunctions.js
@@ -0,0 +1,35 @@
+import recettes from '../../data/recipes.json';
+
+export function findRecipeByName(inputValue, addedIngredientsList, removedIngredientsList) {
+    //Filtrer les recettes dont le nom contient la recherche (recherche par défaut sans ingrédients ajoutés ou retirés)
+    const filteredRecipesByDefault = recettes.recipes.filter(recipe =>
+      recipe.name.toLowerCase().includes(inputValue.toLowerCase())
+    );
+
+    //si addedIngredientsList est vide, retourner les recettes par défaut
+    if (addedIngredientsList.length === 0) return filteredRecipesByDefault;
+    //Filtrer les recettes dont les ingrédients indispensables sont présents
+    console.log('addedIngredientsList : ', addedIngredientsList);
+    
+    const filteredRecipesByAddedIngredients = 
+    filteredRecipesByDefault.filter((recipe) => {
+      console.log("recette testé : ", recipe);
+      for (let i = 0 ; i < addedIngredientsList.length ; i++) {
+        console.log("ingrédient testé : ", addedIngredientsList[i]);
+        if (!recipe.ingredients.map(i => i.name.toLowerCase()).includes(addedIngredientsList[i].toLowerCase())) {
+          return false;
+        }
+    }
+    return true;
+  });
+    console.log("filtered recipes by added ingredients : ", filteredRecipesByAddedIngredients);
+  
+    //Puis, dégager les recettes dont les ingrédients non désirables sont présents
+    return filteredRecipesByAddedIngredients;
+  }
+
+
+  
+  export function findRecipeByIngredient(inputValue, addedIngredientsList, removedIngredientsList){
+     recettes.recipes.filter(recipe => recipe.ingredients.some(ingredient => ingredient.name.toLowerCase().includes(inputValue.toLowerCase())))
+  }
