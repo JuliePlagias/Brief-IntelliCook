597333197826607061c3620da0c6dffa5e4b30ca
GoutteDooo <goudal.Donovan@live.fr>
2025-01-10 09:41:12 +0100

    chore: IngredientsFiltered - Add Hook perso pour plus de clarté

--- src/component/IngredientsDropdown.jsx
+++ src/component/IngredientsDropdown.jsx
@@ -7,33 +7,20 @@ import ingredients from '../data/ingredients.json'
  * @returns 
  */
 const IngredientsDropdown = ({search}) => {
-  const [ingredientsList, setIngredientsList] = useState([])
-  const [addedIngredientsList, setAddedIngredientsList] = useState([])
-  const [removedIngredientsList, setRemovedIngredientsList] = useState([])
-
-  //Retourne la liste des ingrédients qui correspondent à la recherche sous l'input
-  const findIngredients = (value) => {
-    if (!value) return null;
-
-    const newIngredientsListTemp = Object.keys(ingredients).filter(key => {
-      return key.includes(value.toLowerCase());
-    });
-    if (newIngredientsListTemp.length === 0) return null;
-
-    const newIngredientsList = newIngredientsListTemp.filter((newI) => !ingredientsList.includes(newI));
-
-    return newIngredientsList.length ? newIngredientsList : null;
-  }
-
-  const addIngredientToList = (ingredient) => {
-    setIngredientsList([...ingredientsList, ingredient])
-    setAddedIngredientsList([...addedIngredientsList, ingredient])
-  }
-
-  const removeIngredientFromList = (ingredient) => {
-    setIngredientsList([...ingredientsList, ingredient])
-    setRemovedIngredientsList([...removedIngredientsList, ingredient])
-  }
+  
+    //Retourne la liste des ingrédients qui correspondent à la recherche sous l'input
+    const findIngredients = (value) => {
+      if (!value) return null;
+  
+      const newIngredientsListTemp = Object.keys(ingredients).filter(key => {
+        return key.includes(value.toLowerCase());
+      });
+      if (newIngredientsListTemp.length === 0) return null;
+  
+      const newIngredientsList = newIngredientsListTemp.filter((newI) => !ingredientsList.includes(newI));
+  
+      return newIngredientsList.length ? newIngredientsList : null;
+    }
 
   return (findIngredients(search) && (
     <div className='ingredientsDropdown'>
