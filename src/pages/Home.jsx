824b7466ea60fa232fafeef2f328087e0d0e20e0
JuliePlagias <julieplagias@gmail.com>
2025-01-10 13:55:38 +0100

    Add - random facorites component, and changed home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
 import Bloc from '../component/home/Bloc'
-import Footer from '../component/home/Footer'
 import SearchingPage from '../component/SearchingPage'
+import RandomFavorites from '../component/RandomFavorites' 
 
 /**
  * Page d'accueil
  */
 const Home = () => {
+
   return (
     <div className='home'>
       <SearchingPage titlePage={'Accueil'} />
+      <h2>Quelques favoris</h2>
+      <div className='recipes'>
+        <RandomFavorites number={3}/>
+      </div>
       <Bloc />
     </div>
   )
