1ae90245b2c4faaa9d2ae67fd60a000d57cd4cee
JuliePlagias <julieplagias@gmail.com>
2025-01-13 11:53:43 +0100

    bloc dark mode

--- src/component/home/Bloc.jsx
+++ src/component/home/Bloc.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from 'react'
+import { DarkModeContext } from '../DarkModeProvider'
 
 const Bloc = () => {
+  const { darkMode } = useContext(DarkModeContext)
   return (
-    <div className="home__bloc">
+    <div className={`home__bloc ${darkMode && 'blocDark'}`}>
       Bienvenue sur Intellicook, votre complice en cuisine ! Découvrez des
       recettes délicieuses, simples et inspirantes, conçues pour ravir vos
       papilles et sublimer vos moments gourmands. Que vous soyez chef en herbe
@@ -10,7 +12,7 @@ const Bloc = () => {
       transformer chaque repas en une expérience inoubliable. avec Intellicook
       vous pouvez partager vos recettes de cuisine
     </div>
-  );
-};
+  )
+}
 
-export default Bloc;
+export default Bloc
