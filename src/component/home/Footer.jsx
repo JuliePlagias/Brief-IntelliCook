bcd0233b75200e6fe93a974d9bc4ba82af793d60
JuliePlagias <julieplagias@gmail.com>
2025-01-13 11:46:40 +0100

    Footer dark mode

--- src/component/home/Footer.jsx
+++ src/component/home/Footer.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { DarkModeContext } from '../DarkModeProvider'
 
 export default function Footer() {
+  const { darkMode } = useContext(DarkModeContext)
   return (
-    <div className={`footer`}>
-      <Link to='/catalogue'><p>Catalogue</p></Link>
-      <Link to="/conditions"><p>Conditions d'utilisation</p></Link>
+    <div className={`footer ${darkMode && 'footerDark'}`}>
+      <Link to='/catalogue'>
+        <p>Catalogue</p>
+      </Link>
+      <Link to='/conditions'>
+        <p>Conditions d'utilisation</p>
+      </Link>
       <p>@Copyright Intellicook tous droits réservés</p>
     </div>
   )
